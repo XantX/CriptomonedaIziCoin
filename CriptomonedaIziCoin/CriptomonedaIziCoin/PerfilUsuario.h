fe9bbcdfbe191f94d7ee3a7083b457526dab23a4
Sebastian <u201717471@upc.edu.pe>
2020-04-28 22:11:27 -0500

    graficos

--- CriptomonedaIziCoin/CriptomonedaIziCoin/PerfilUsuario.h
+++ CriptomonedaIziCoin/CriptomonedaIziCoin/PerfilUsuario.h
@@ -36,13 +36,20 @@ namespace CriptomonedaIziCoin {
 		}
 	private: System::Windows::Forms::Button^  button1;
 	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  CantIziCoins;
+
 	private: System::Windows::Forms::ListView^  listView1;
 	private: System::Windows::Forms::Label^  Transaccion_titulo;
 	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::Label^  CantDolares;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+
 
 	protected:
 
@@ -59,22 +66,25 @@ namespace CriptomonedaIziCoin {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(PerfilUsuario::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->CantIziCoins = (gcnew System::Windows::Forms::Label());
 			this->listView1 = (gcnew System::Windows::Forms::ListView());
 			this->Transaccion_titulo = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->CantDolares = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(254, 458);
+			this->button1->Location = System::Drawing::Point(541, 233);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(179, 23);
 			this->button1->TabIndex = 0;
@@ -85,50 +95,30 @@ namespace CriptomonedaIziCoin {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(202, 35);
+			this->label1->Location = System::Drawing::Point(182, 61);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(43, 13);
+			this->label1->Size = System::Drawing::Size(385, 13);
 			this->label1->TabIndex = 1;
-			this->label1->Text = L"Usuario";
+			this->label1->Text = L"58a01e1e4103f19c6fb271465e7c280b2bb6b44e0ab43e607bd520e5b3a4a1b1";
 			this->label1->Click += gcnew System::EventHandler(this, &PerfilUsuario::label1_Click);
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(167, 64);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(112, 117);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-			this->pictureBox1->TabIndex = 2;
-			this->pictureBox1->TabStop = false;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(131, 190);
+			this->label2->Location = System::Drawing::Point(610, 27);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(69, 24);
 			this->label2->TabIndex = 3;
 			this->label2->Text = L"IziCoins:";
 			this->label2->Click += gcnew System::EventHandler(this, &PerfilUsuario::label2_Click);
 			// 
-			// CantIziCoins
-			// 
-			this->CantIziCoins->AutoSize = true;
-			this->CantIziCoins->Location = System::Drawing::Point(237, 197);
-			this->CantIziCoins->Name = L"CantIziCoins";
-			this->CantIziCoins->Size = System::Drawing::Size(65, 13);
-			this->CantIziCoins->TabIndex = 4;
-			this->CantIziCoins->Text = L"CantIziCoins";
-			this->CantIziCoins->Click += gcnew System::EventHandler(this, &PerfilUsuario::label3_Click);
-			// 
 			// listView1
 			// 
-			this->listView1->Location = System::Drawing::Point(46, 279);
+			this->listView1->Location = System::Drawing::Point(185, 284);
 			this->listView1->Name = L"listView1";
-			this->listView1->Size = System::Drawing::Size(367, 162);
+			this->listView1->Size = System::Drawing::Size(567, 179);
 			this->listView1->TabIndex = 5;
 			this->listView1->UseCompatibleStateImageBehavior = false;
 			// 
@@ -137,7 +127,7 @@ namespace CriptomonedaIziCoin {
 			this->Transaccion_titulo->AutoSize = true;
 			this->Transaccion_titulo->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->Transaccion_titulo->Location = System::Drawing::Point(42, 245);
+			this->Transaccion_titulo->Location = System::Drawing::Point(181, 236);
 			this->Transaccion_titulo->Name = L"Transaccion_titulo";
 			this->Transaccion_titulo->Size = System::Drawing::Size(195, 20);
 			this->Transaccion_titulo->TabIndex = 6;
@@ -148,34 +138,92 @@ namespace CriptomonedaIziCoin {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(131, 214);
+			this->label3->Location = System::Drawing::Point(610, 61);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(68, 24);
 			this->label3->TabIndex = 7;
 			this->label3->Text = L"Dolares:";
 			this->label3->Click += gcnew System::EventHandler(this, &PerfilUsuario::label3_Click_1);
 			// 
-			// CantDolares
+			// label4
 			// 
-			this->CantDolares->AutoSize = true;
-			this->CantDolares->Location = System::Drawing::Point(237, 221);
-			this->CantDolares->Name = L"CantDolares";
-			this->CantDolares->Size = System::Drawing::Size(65, 13);
-			this->CantDolares->TabIndex = 8;
-			this->CantDolares->Text = L"CantDolares";
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(708, 34);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(44, 13);
+			this->label4->TabIndex = 9;
+			this->label4->Text = L"6,00 izc";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(697, 68);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(55, 13);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"$18,332.4";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(182, 27);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(59, 13);
+			this->label6->TabIndex = 11;
+			this->label6->Text = L"Tu Codigo:";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::HotTrack;
+			this->pictureBox1->Location = System::Drawing::Point(0, 1);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(162, 494);
+			this->pictureBox1->TabIndex = 12;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 63);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(137, 23);
+			this->button2->TabIndex = 13;
+			this->button2->Text = L"Transacciones";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(12, 440);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(137, 23);
+			this->button3->TabIndex = 14;
+			this->button3->Text = L"Cerra Sesion";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(204, 97);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 15;
+			this->button4->Text = L"Copiar";
+			this->button4->UseVisualStyleBackColor = true;
 			// 
 			// PerfilUsuario
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(445, 493);
-			this->Controls->Add(this->CantDolares);
+			this->ClientSize = System::Drawing::Size(794, 493);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->Transaccion_titulo);
 			this->Controls->Add(this->listView1);
-			this->Controls->Add(this->CantIziCoins);
 			this->Controls->Add(this->label2);
-			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"PerfilUsuario";
