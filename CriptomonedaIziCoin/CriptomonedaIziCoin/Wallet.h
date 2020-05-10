35f4fa1efc30b184053bead3bceec162de882a51
gonzaloLaqui147 <55416564+gonzaloLaqui147@users.noreply.github.com>
2020-05-10 00:15:35 -0500

    Update Wallet.h

--- CriptomonedaIziCoin/CriptomonedaIziCoin/Wallet.h
+++ CriptomonedaIziCoin/CriptomonedaIziCoin/Wallet.h
@@ -11,15 +11,27 @@ class Wallet {
 	public:
 		string HashCode;
 
-		Wallet(string correo, string contraseña) {
+		Wallet(string correo, string contraseÃ±a) {
 			IziCoins = 0;
-			HashCode = SHA256::cifrar(correo + contraseña);
-			
+			HashCode = SHA256::cifrar(correo + contraseÃ±a);
+			CpublicN = 0;
 		}
 		void setIzicoins(double Coins) {
 			IziCoins = IziCoins + Coins;
 		}
+
+		void setCpublicN(long _cpn) { this->CpublicN = _cpn; }
+		
+		void setCpublicE(long _cpe) { this->CpublicE = _cpe; }
+
+		void setCprivadaD(long _cpd) { this->CprivadaD = _cpd; }
+
 		double getIzicoins() { return IziCoins; }
+
+		long getCpublicN()  { return CpublicN;  }
+		long getCpublicE()  { return CpublicE;  }
+		long getCprivadaD() { return CprivadaD; }
+
 		void GenerarTran(double IziCoins, string destino);
 		Transaccion* getTransac();
 		void RecibeUnaTransaccion(Transaccion *Recivido);
